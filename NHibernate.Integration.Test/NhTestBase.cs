5f183283fef843b2377203af404417efcc702e40
TheHunter <chico_herbal@hotmail.com>
2014-05-07 01:32:10 -0700

    Aggiornamento massivo

--- NHibernate.Integration.Test/NhTestBase.cs
+++ NHibernate.Integration.Test/NhTestBase.cs
@@ -36,10 +36,8 @@ namespace NHibernate.Integration.Test
 
             if (BeforeBuilding != null)
                 this.BeforeBuilding.Invoke(cfg);
-            
 
             sessionFactory = cfg.BuildSessionFactory();
-
         }
 
         /// <summary>
